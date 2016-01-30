5ac96f89f2750f450ad1f6d3713ea4995a9d8d5d
pitrud <prudnicki@plog.pl>
2016-01-30 13:49:10 +0100

    poprawki

--- zadanie3.playground/Contents.swift
+++ zadanie3.playground/Contents.swift
@@ -2,6 +2,16 @@
 
 import UIKit
 
+//tablica z danymi różnych fiszek
+var tablica_danych = [[Any]]()
+tablica_danych.append(["s1_strona1","s1_strona2","c","d",123])
+tablica_danych.append(["s2_strona1","s2_strona2","c","d",123])
+tablica_danych.append(["s3_strona1","s3_strona2","c","d",123])
+tablica_danych.append(["s4_strona1","s4_strona2","c","d",123])
+
+
+
+
 protocol daSie {
     var czySieDa:Bool {get set}
 }
@@ -83,46 +93,81 @@ class fiszki: CustomStringConvertible, CustomDebugStringConvertible, daSie {
     
 }
 
-//tablica z danymi różnych fiszek
-var tablica_danych = [[Any]]()
-tablica_danych.append(["s1_strona1","s1_strona2","c","d",123])
-tablica_danych.append(["s2_strona1","s2_strona2","c","d",123])
-tablica_danych.append(["s3_strona1","s3_strona2","c","d",123])
-tablica_danych.append(["s4_strona1","s4_strona2","c","d",123])
 
-var tab_fiszek = [fiszki]()
+protocol roz:SequenceType {
+    
+}
 
-//UZUPEŁNIJ FISZKI JAKIMIŚ DANYMI
+
+
+class ogarnijFiszki:SequenceType {
+    var tab_fiszek = [fiszki]()
+    
+    
+    var fileArr:[String] = ["a.swift","b.swift","c.swift"]
+    
+    //pamiętaj:
+    //func generate() -> GeneratorType
+    func generate() -> AnyGenerator<Any> {
+        var iloscElementow = self.tab_fiszek.count
+        //http://swiftdoc.org/v2.1/func/anyGenerator/#func-anygenerator-element_-element
+
+        return anyGenerator {
+                return iloscElementow>0&&self.tab_fiszek[--iloscElementow].czyUmie() ? self.tab_fiszek[iloscElementow] : nil
+        }
+
+    }
+
+
+    func pokaz() {
+        print(tab_fiszek[0].czyUmie())
+    }
+    
+    func dodajFiszke(fiszka: fiszki) {
+        tab_fiszek.append(fiszka)
+    }
+    
+    func nauczLosowe() {
+        for fiszka in tab_fiszek {
+            if arc4random_uniform(2)==1 {
+                fiszka.nauczMnie()
+            }
+        }
+    }
+    
+    func wyswietlNienauczone() {
+        for fiszka in tab_fiszek {
+            if fiszka.czyUmie() {
+                print("\nmożesz się jeszcze nauczyć fiszki \(fiszka)")
+                //fiszka.wybierzTypOdpowiedzi(.cyferki)
+                //print("\nPodpowiedź dla tej fiszki: \(fiszka)")
+            }
+            
+        }
+    }
+
+}
+
+var tablica = ogarnijFiszki()
+
+//UZUPEŁNIJ FISZKI DANYMI
 for tmp in tablica_danych {
     let jfiszka = fiszki(
             strona1: String(tmp[0]),
             strona2: String(tmp[1]),
             podpowiedzi: [0:tmp[2], 1:tmp[3], 2:tmp[4]]
     )
-    tab_fiszek.append(jfiszka)
+    tablica.dodajFiszke(jfiszka)
 }
 
-//..i ustaw losowe fiszki jako nauczone
-for fiszka in tab_fiszek {
-    if arc4random_uniform(2)==1 {
-        fiszka.nauczMnie()
-    }
-}
-
-//wyświetl jeszcze nie nauczone
-for fiszka in tab_fiszek {
-    if fiszka.czyUmie() {
-        print("\nmożesz się jeszcze nauczyć fiszki \(fiszka)")
-        fiszka.wybierzTypOdpowiedzi(.cyferki)
-        print("\nPodpowiedź dla tej fiszki: \(fiszka)")
-    }
-    
-}
-
-/*
-func poka(el:fiszki)->SequenceType {
-    
-}
-*/
-
+tablica.pokaz()
+tablica.nauczLosowe()
+tablica.wyswietlNienauczone()
+print("a ponizej na podstawie anygenerator;SequenceType;next()")
+var oko = tablica.generate()
+oko.next()
+oko.next()
+oko.next()
+oko.next()
+oko.next()
 print("koniec")
